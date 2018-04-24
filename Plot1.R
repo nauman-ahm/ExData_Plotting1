ad521a8aa464a67dbd565f60b0d88b24dfb5e088
nauman-ahm <35228011+nauman-ahm@users.noreply.github.com>
2018-04-24 15:45:59 -0400

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,29 @@
+library("lubridate")
+if (!file.exists("household_power_consumption.txt"))
+{
+  if (!file.exists("household_power_consumption.zip"))
+  {
+    download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "household_power_consumption.zip")
+  }
+  unzip("household_power_consumption.zip")
+}
+data<-read.table("household_power_consumption.txt", sep = ";", header = TRUE)
+data$Date<-as.Date(data$Date, "%d/%m/%Y")
+data<- data[((data$Date == "2007-02-01")|(data$Date == "2007-02-02")) ,]
+data$Global_active_power<-as.character(data$Global_active_power)
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Sub_metering_1<-as.character(data$Sub_metering_1)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.character(data$Sub_metering_2)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Sub_metering_3<-as.character(data$Sub_metering_3)
+data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
+data$Time<-as.character(data$Time)
+data$DateTime <- paste(data$Date, data$Time)
+data$DateTime<-ymd_hms(data$DateTime)
+
+## PLOT 1
+hist(data$Global_active_power, main = "Global Active Power", col = "red", 
+     xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+dev.copy(png,"plot1.png")
+dev.off()
